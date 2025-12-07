214c424851c229b2e1922511fa9e05a045158a97
Sergei <serg.ivnv05@gmail.com>
2025-12-06 20:55:07 -0500

    Switch to app.config.js with env API key

---
+++ app.config.js
@@ -0,0 +1,84 @@
+export default {
+  expo: {
+    name: "Rybolov",
+    slug: "fishingapp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "fishingapp",
+    userInterfaceStyle: "automatic",
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSLocationWhenInUseUsageDescription: "We use your location to center the fishing map around you.",
+        NSPhotoLibraryUsageDescription: "Allow $(PRODUCT_NAME) to access your photos to extract GPS data.",
+        NSCameraUsageDescription: "Allow $(PRODUCT_NAME) to access your camera."
+      },
+      bundleIdentifier: "com.anonymous.fishingapp"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      permissions: [
+        "ACCESS_FINE_LOCATION",
+        "ACCESS_COARSE_LOCATION",
+        "ACCESS_MEDIA_LOCATION",
+        "READ_MEDIA_IMAGES",
+        "READ_EXTERNAL_STORAGE",
+        "CAMERA"
+      ],
+      config: {
+        googleMaps: {
+          apiKey: process.env.API_KEY
+        }
+      },
+      package: "com.anonymous.fishingapp"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ],
+      "expo-maps",
+      "expo-web-browser",
+      "expo-sqlite",
+      [
+        "expo-media-library",
+        {
+          photosPermission: "Allow $(PRODUCT_NAME) to access your photos.",
+          savePhotosPermission: "Allow $(PRODUCT_NAME) to save photos.",
+          isAccessMediaLocationEnabled: true,
+          isMicrophonePermissionDeclared: false
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "b4647e00-4478-4b12-b489-a7a8d98f70f4"
+      }
+    },
+    runtimeVersion: "1.0.0",
+    updates: {
+      enabled: true,
+      fallbackToCacheTimeout: 0
+    }
+  }
+};
